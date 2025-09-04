10215332df29f443aad54c265941b70ec79c1449
Krushovice <kickstar69@yandex.ru>
2025-09-04 17:55:25 +1000

    add new changes to build app func

--- src/backend/web/factory.py
+++ src/backend/web/factory.py
@@ -9,21 +9,20 @@ from core.config import settings
 from backend.web.lifespan import make_lifespan
 from backend.web.routers.webhook import make_webhook_router
 from backend.web.routers.front import router as front_router
+from backend.web.routers.api import router as api_router
 
 
 def create_app() -> FastAPI:
-    # 1) Собираем bot/dp
+
     bot = Bot(
         token=settings.bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
     dp = Dispatcher()
 
-    # 2) Общие параметры вебхука
     webhook_path = "/tg/webhook"
     secret = settings.web.webhook_header_secret or None
 
-    # 3) FastAPI с вынесенным lifespan
     app = FastAPI(
         lifespan=make_lifespan(
             bot=bot,
@@ -42,8 +41,8 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-    # 4) Роутер, который знает только о dp/bot и пути
     app.include_router(front_router)
+    app.include_router(api_router)
     app.include_router(
         make_webhook_router(
             dp=dp,
