859df9a365a0737a9ca3b5843b618e7b2a12ed9e
Krushovice <kickstar69@yandex.ru>
2025-09-04 17:15:21 +1000

    build main script

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from backend.create_app import create_app
+from core.config import settings
+
+app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    # Запускаем сервер. Telegram будет стучаться на /tg/webhook/<секрет>
+    uvicorn.run(
+        "main:app",
+        host=settings.uvicorn.host,
+        port=settings.uvicorn.port,
+        reload=True,
+    )
